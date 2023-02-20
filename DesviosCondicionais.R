073ee1bc831f68e2cb552c902fbf955e70fd9b9e
Isaac Sebastian <sisaac933@gmail.com>
2023-02-20 17:20:39 -0300

    tests

---
+++ DesviosCondicionais.R
@@ -0,0 +1,27 @@
+x <- 31
+if(x == 30){
+  print("entrou")
+}else{
+  print("não entrou")
+}
+
+x<-0
+
+while(x <= 10){
+  print(x)
+  x <- x + 1
+  if(x == 10){
+    print("Esse laço chegou ao fim")
+  }
+}
+
+teste = readline(prompt = "Digite seu nome: ")
+Bob Esponja
+
+numero = as.numeric(readline("Digite um número: "))
+1
+
+radiuos = as.numeric(readline())
+3
+volume <- (4/3) * 3.14 * radiuos * radiuos * radiuos
+cat("VOLUME = ", volume)
